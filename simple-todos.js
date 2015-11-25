46952e34ddc7fb41da12ee87589993734eae0494
Nick Brennan <nick_p_b@yahoo.com>
2015-11-24 20:25:05 -0800

    the C in CRUD

--- simple-todos.js
+++ simple-todos.js
@@ -1,12 +1,26 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"}
-    ]
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }   
+  });
+
+  Template.body.events({
+    "submit .new-task": function(e){
+      e.preventDefault();
+      var text = e.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      e.target.text.value = "";
+    }
   });
 }
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
